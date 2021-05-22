b3b74480349c0da8c8973cf9a2b7fbed0c5b2ea0
Lukasz <Lother367@gmail.com>
2021-05-22 10:00:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# import math
+#
+# import pytest
+#
+#
+# first_number = input("podaj liczbe")
+# second_number= input("podaj druga liczbe")
+# x = int(first_number)
+# y = int(second_number)
+#
+# def dodawanie():
+#     print("Wynik dodawania:")
+#     print(x+y)
+#
+# def odejmowanie():
+#     print("Wynik odejmowania:")
+#     print(x-y)
+#
+# def mnożenie():
+#     print("Wynik mnożenia:")
+#     print(x*y)
+#
+# def dzielenie():
+#     print("Wynik dzielenia:")
+#     print(x/y)
+#
+#
+# def liczby_pierwsze():
+#     print("Liczby pierwsze to:")
+#     for number in range (1, 100):
+#         if number == 1: continue
+#         if number == 2 or number == 3:
+#             print(number)
+#         if number < 10 and number % 2 > 0 and number % 3 > 0 and number % 4 > 0:
+#             print(number)
+#         if number % 2 > 0 and  number % 3 > 0 and number % 4 > 0 and number % 5 > 0 and number % 6 > 0 and number % 7 > 0 :
+#             print(number)
+#
+#
+#  dodawanie(), odejmowanie(), mnożenie(), dzielenie(), liczby_pierwsze()
+
+
